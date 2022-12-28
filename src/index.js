8193565cef033dbbeb733b24994383807aeefd87
Antonio Bruno <abruno@artiso.com>
2022-12-28 11:58:20 +0100

    still refactoring

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 // import InfoJobMain from './InfoJob';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import MainInfo from './mainInfo';
+import MainInfo from './components/mainInfo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
