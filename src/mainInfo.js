c674c53730441dee36c60a3a36b811a5e25ec340
Antonio Bruno <abruno@artiso.com>
2022-12-28 10:33:10 +0100

    mainInfo component has the "main" state

--- src/mainInfo.js
+++ src/mainInfo.js
@@ -1,13 +1,17 @@
 import { TableInfo } from "./tableInfo";
 import { PeriodoInfo } from "./periodoInfo";
+import { useState, useEffect } from "react";
+import {SetInitialControlData} from "./Utility";
 
 export default function MainInfo(props){
+
+    const [data, setData] = useState(SetInitialControlData());
 
     return (
     <div className="container">
         <div className="row mt-3 p-3">
-            <TableInfo newdata={props.data}/>
-            <PeriodoInfo newdata={props.data} />
+            <TableInfo newdata={data}/>
+            <PeriodoInfo newdata={data} onChange={setData} />
         </div>
     </div>        
 )
